37d48f7dfba42960d8d30bd79b79579345366829
Hayashi322 <theethawat.muak@bumail.net>
2024-02-07 02:06:52 +0700

    VFX Move

--- Assets/Script/Command/UnitCommand.cs
+++ Assets/Script/Command/UnitCommand.cs
@@ -42,6 +42,7 @@ public class UnitCommand : MonoBehaviour
     private void CommandToGround(RaycastHit hit, Units unit)
     {
         UnitsMoveToPosition(hit.point, unit);
+        CreateVFXMarker(hit.point, MainUI.instance.SelectionMarker);
     }
 
     private void TryCommand(Vector2 screenPos)
@@ -59,5 +60,13 @@ public class UnitCommand : MonoBehaviour
                     break;
             }
         }
+    }
+
+    private void CreateVFXMarker(Vector3 pos, GameObject vfxPrefab)
+    {
+        if (vfxPrefab == null)
+            return;
+
+        Instantiate(vfxPrefab, new Vector3(pos.x, 0.1f, pos.z), Quaternion.identity);
     }
 }
